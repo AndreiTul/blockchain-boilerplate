10101f2fd57504aff67691a3b0e630b6a589ac47
Andrei Tulbure <andreitulbure123@gmail.com>
2021-07-07 11:43:13 +0300

    31. Test the Nonce Functionality

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,7 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
+const { DIFFICULTY } = require('../config');
 
-
-const DIFFICULTY = 4;
 class Block {
     constructor(timestamp, lastHash, hash, data, nonce) {
         this.timestamp = timestamp;
@@ -31,11 +30,12 @@ class Block {
 
         do {
             nonce++;
+            timestamp = Date.now();
             hash = Block.hash(timestamp, lastHash, data, nonce);
         } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
         
 
-        return new this(timestamp, lastHash, hash, data,);
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
     static hash(timestamp, lastHash, data, nonce) {
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
     let data, lastBlock, block;
@@ -18,6 +19,7 @@ describe('Block', () => {
     });
 
     it('generates a hash that matches the difficulty', () => {
-
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log(block.toString());
     });
 });
